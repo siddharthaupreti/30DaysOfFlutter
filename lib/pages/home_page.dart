9b2c4a0ae1d8381a4b1e31bf8d0a54c8552d7e76
Siddhartha Upreti <sid876upreti@gmail.com>
2022-01-16 23:51:02 +0545

    item 1 displayed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
+import 'package:codepur/models/catalog.dart';
 import 'package:codepur/widgets/drawer.dart';
+import 'package:codepur/widgets/item_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -8,14 +10,20 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          "Catalog App",
-          style: TextStyle(color: Colors.black),
-        ),
-      ),
-      body: Center(
-        child: Container(
-          child: Text("Welcome to 30 days of flutter"),
+          title: const Text(
+            "Catalog App",
+            style: TextStyle(color: Colors.black),
+          ),
+          centerTitle: true),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: CatalogModel.items.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: CatalogModel.items[index],
+            );
+          },
         ),
       ),
       drawer: MyDrawer(),
